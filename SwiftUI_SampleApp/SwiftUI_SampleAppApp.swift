39f78b57a687fea6bb3bf9de3a0220ec1110bd65
Yusuf Keskin <yusuf_keskin@outlook.com>
2023-04-10 20:16:20 +0300

    Initial Commit

---
+++ SwiftUI_SampleApp/SwiftUI_SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SampleAppApp.swift
+//  SwiftUI_SampleApp
+//
+//  Created by YUSUF KESKİN on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
